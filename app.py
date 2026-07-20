99c98c851ad17351d1d7d6ae95a595f9ccd1b0d2
Deahtly <ciaoswd@gmail.com>
2026-07-19 22:06:26 -0600

    cambio configuracion de flask para render

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask,render_template
-app=Flask(__name__)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 @app.route('/')
-def i(): return render_template('index.html')
-app.run(debug=True)
+def i():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
